f569597de1bcade1648105d81d9ee019c2ee3a53
Igor Follador <igorledf@hotmail.com>
2024-08-16 19:34:59 -0300

    fix: - Bugfix descriptografia do token JWT

--- src/shared/guards/auth.guard.ts
+++ src/shared/guards/auth.guard.ts
@@ -28,7 +28,7 @@ export class AuthGuard implements CanActivate {
 
     try {
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: 'batman',
+        secret: process.env.JWT_SECRETS,
       });
 
       request['usuario'] = payload;
