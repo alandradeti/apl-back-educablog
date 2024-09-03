00029c1a98a8ae096e4efcbe6d37aadc4219b570
Igor Follador <igorledf@hotmail.com>
2024-09-02 22:58:58 -0300

    feat: - Configurando CORS para consumo do frontend

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ import { GlobalExceptionFilter } from './shared/filters/global.exception.filter'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //Configuração do filtro de exceção global
+  // Configuração do filtro de exceção global
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new GlobalExceptionFilter(httpAdapter));
 
-  //Configuração do Swagger
+  // Configuração do Swagger
   const config = new DocumentBuilder()
     .setTitle('Educablog-API')
     .setDescription('Educablog API - Gestão de postagens escolares')
@@ -21,8 +21,17 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); //Inicialização do Swagger (Documetação/Requisição) da API
-  setupRedoc(app); //Documetação detalhada da API
+  SwaggerModule.setup('api', app, document); // Inicialização do Swagger (Documentação/Requisição) da API
+  setupRedoc(app); // Documentação detalhada da API
+
+  // Configuração do CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
+
   await app.listen(Number(process.env.PORT_APP) || 3010);
 }
 bootstrap();
