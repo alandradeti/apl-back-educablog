97c130a2396c4174b80df54fe6b76cbdfc40c349
Igor Follador <igorledf@hotmail.com>
2024-09-20 14:22:35 -0300

    feat: - Add a new header x-total-count to return the total items per page

--- src/post/repositories/pg/post.pg.repository.ts
+++ src/post/repositories/pg/post.pg.repository.ts
@@ -7,10 +7,13 @@ import { PostRepository } from '../post.repository';
 export class PostPgRepository implements PostRepository {
   constructor(@InjectRepository(Post) private repository: Repository<Post>) {}
 
-  async findAll(limite: number, pagina: number): Promise<IPost[] | null> {
+  async findAll(
+    limite: number,
+    pagina: number,
+  ): Promise<{ data: IPost[]; totalCount: number } | null> {
     const maxLimite = Math.min(limite, 50);
 
-    return this.repository.find({
+    const [data, totalCount] = await this.repository.findAndCount({
       relations: ['categoria', 'usuarioCriacao', 'usuarioAtualizacao'],
       select: {
         usuarioCriacao: {
@@ -23,34 +26,43 @@ export class PostPgRepository implements PostRepository {
         },
       },
       skip: (pagina - 1) * maxLimite,
-      take: limite,
+      take: maxLimite,
     });
+
+    return { data, totalCount };
   }
 
-  async findAllActive(limite: number, pagina: number): Promise<IPost[] | null> {
+  async findAllActive(
+    limite: number,
+    pagina: number,
+  ): Promise<{ data: IPost[]; totalCount: number } | null> {
     const maxLimite = Math.min(limite, 50);
 
-    return this.repository.find({
+    const [data, totalCount] = await this.repository.findAndCount({
       relations: ['categoria'],
       where: { ativo: true },
       skip: (pagina - 1) * maxLimite,
-      take: limite,
+      take: maxLimite,
     });
+
+    return { data, totalCount };
   }
 
   async findAllPostCategoria(
     limite: number,
     pagina: number,
     idCategoria: string,
-  ): Promise<IPost[]> {
+  ): Promise<{ data: IPost[]; totalCount: number }> {
     const maxLimite = Math.min(limite, 50);
 
-    return this.repository.find({
+    const [data, totalCount] = await this.repository.findAndCount({
       relations: ['categoria'],
       where: { categoria: { id: idCategoria } },
       skip: (pagina - 1) * maxLimite,
-      take: limite,
+      take: maxLimite,
     });
+
+    return { data, totalCount };
   }
 
   async findById(id: string): Promise<IPost | null> {
@@ -60,13 +72,21 @@ export class PostPgRepository implements PostRepository {
     });
   }
 
-  async search(query: string): Promise<IPost[] | null> {
-    return this.repository
+  async search(
+    query: string,
+    includeInactive: boolean,
+  ): Promise<{ data: IPost[]; totalCount: number }> {
+    const qb = this.repository
       .createQueryBuilder('post')
-      .where('post.ativo = :ativo', { ativo: true })
-      .andWhere('post.titulo ILIKE :query', { query: `%${query}%` })
-      .orWhere('post.descricao ILIKE :query', { query: `%${query}%` })
-      .getMany();
+      .where('post.titulo ILIKE :query', { query: `%${query}%` })
+      .orWhere('post.descricao ILIKE :query', { query: `%${query}%` });
+
+    if (!includeInactive) {
+      qb.andWhere('post.ativo = :ativo', { ativo: true });
+    }
+
+    const [data, totalCount] = await qb.getManyAndCount();
+    return { data, totalCount };
   }
 
   async create(post: IPost): Promise<IPost | null> {
--- src/post/repositories/post.repository.ts
+++ src/post/repositories/post.repository.ts
@@ -1,18 +1,24 @@
 import { IPost } from '../entities/interfaces/post.interface';
 
 export abstract class PostRepository {
-  abstract findAll(limite: number, pagina: number): Promise<IPost[] | null>;
+  abstract findAll(
+    limite: number,
+    pagina: number,
+  ): Promise<{ data: IPost[]; totalCount: number }>;
   abstract findAllActive(
     limite: number,
     pagina: number,
-  ): Promise<IPost[] | null>;
+  ): Promise<{ data: IPost[]; totalCount: number }>;
   abstract findAllPostCategoria(
     limite: number,
     pagina: number,
     idCategoria: string,
-  ): Promise<IPost[] | null>;
+  ): Promise<{ data: IPost[]; totalCount: number }>;
   abstract findById(id: string): Promise<IPost | null>;
-  abstract search(query: string): Promise<IPost[] | null>;
+  abstract search(
+    query: string,
+    includeInactive: boolean,
+  ): Promise<{ data: IPost[]; totalCount: number }>;
   abstract create(post: IPost): Promise<IPost | null>;
   abstract update(post: IPost): Promise<IPost | null>;
   abstract delete(id: string): Promise<void>;
